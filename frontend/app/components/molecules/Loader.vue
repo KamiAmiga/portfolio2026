66a0ab4b4ef4486596b1cfb1c97bc83d01e29c73
KamiAmiga <contact@cgicquel.fr>
2026-01-14 22:57:02 +0100

    Loader component

---
+++ frontend/app/components/molecules/Loader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+<div class="loader">
+  <Logo />
+</div>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/styles/components/molecules/loader' as *;
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  components: {
+    dirs: [
+      {
+        path: "~/components/atoms",
+        global: true,
+      },
+      {
+        path: "~/components/molecules",
+        global: true,
+      },
+      "~/components",
+    ],
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/content']
 })
