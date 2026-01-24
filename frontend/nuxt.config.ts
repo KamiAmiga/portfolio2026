39b77599259732eb46fd3d7c9842bdea33b92ee1
KamiAmiga <contact@cgicquel.fr>
2026-01-24 16:54:16 +0100

    Favicon and config updates

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,35 +2,22 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'Camille Gicquel - Portfolio',
+      htmlAttrs: {
+        lang: 'fr-FR'
+      },
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
-      htmlAttrs: {
-        lang: 'fr-FR'
-      }
+      title: 'Camille Gicquel - Portfolio',
     }
   },
   compatibilityDate: '2025-07-15',
-  components: {
-    dirs: [
-      {
-        path: "~/components/atoms",
-        global: true,
-      },
-      {
-        path: "~/components/molecules",
-        global: true,
-      },
-      {
-        path: "~/components/organisms",
-        global: true,
-      },
-      "~/components",
-    ],
-  },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   devtools: { enabled: true },
   image: {
     screens: {
