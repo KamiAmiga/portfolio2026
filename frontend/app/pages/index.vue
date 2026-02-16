e46535618de32ab9a459235aa9df963d4b39684d
KamiAmiga <contact@cgicquel.fr>
2026-02-16 22:23:17 +0100

    Base homepage illu

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -25,10 +25,36 @@ if (seoMeta) {
 </script>
 
 <template>
-  <template v-if="homeData">
-    {{ homeData.title }}
-  </template>
+  <div v-if="homeData">
+    <Grid splitting="full" class="title-wrapper">
+      <h1 class="title">
+        <div class="title__small font-sans--md-capitalized">{{ homeData.title }}</div>
+        <div class="title__large font-sans--2xl">
+          {{ homeData.subtitle }}</div>
+      </h1>
+    </Grid>
+
+    <div class="illustration">
+      <div class="illustration__cityscape">
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-1" />
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-2" />
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-3" />
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-4" />
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-5" />
+        <div class="illustration__cityscape__shape illustration__cityscape__shape--back-6" />
+      </div>
+
+      <div class="illustration__ground">
+        <div class="illustration__ground__shape"></div>
+      </div>
+
+      <div class="illustration__logo-wrapper">
+        <Logo />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@use '../assets/styles/pages/homepage' as *;
 </style>
