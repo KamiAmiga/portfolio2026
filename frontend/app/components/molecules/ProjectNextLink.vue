cdd87974407fe6a338e17d3ceff2d4c3d3060921
KamiAmiga <contact@cgicquel.fr>
2026-07-16 21:47:43 +0200

    Update ProjectNextLink hover and focus anim

--- frontend/app/components/molecules/ProjectNextLink.vue
+++ frontend/app/components/molecules/ProjectNextLink.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 import type { ProjectsCollectionItem } from "@nuxt/content";
 
 defineProps<{
@@ -10,6 +11,16 @@ defineProps<{
 
 const projectNextLink = useTemplateRef('projectNextLink')
 const loadAssets = useAssetsLoading(projectNextLink)
+
+const handleHoverFocus = () => {
+  if (!splittedTitleTimeline || !splittedOvertitleTimeline) return
+
+  splittedTitleTimeline().play(0)
+  splittedOvertitleTimeline().play(0)
+}
+
+let splittedOvertitleTimeline: () => gsap.core.Timeline
+let splittedTitleTimeline: () => gsap.core.Timeline
 
 useGSAP((isReducedMotion, context) => {
   if (isReducedMotion) {
@@ -22,23 +33,62 @@ useGSAP((isReducedMotion, context) => {
       once: true,
     }
   })
+
+  const splitOvertitle = SplitText.create(context.selector?.('.project-next-link__label__overtitle'), {
+    type: "words",
+    mask: "lines",
+    aria: "hidden"
+  });
+
+  const splitTitle = SplitText.create(context.selector?.('.project-next-link__label__title'), {
+    type: "chars,words",
+    mask: "lines",
+    aria: "hidden"
+  });
+
+  splittedOvertitleTimeline = () => {
+    const timeline = gsap.timeline()
+
+    timeline
+      .from(splitOvertitle.words, {
+        y: '100%',
+        duration: .4,
+        ease: "power3.inOut",
+        stagger: .05
+      })
+
+    return timeline
+  }
+
+  splittedTitleTimeline = () => {
+    const timeline = gsap.timeline()
+
+    timeline
+      .from(splitTitle.chars, {
+        opacity: 0,
+        y: '100%',
+        duration: .3,
+        ease: "power2.inOut",
+        stagger: .02
+      })
+
+    return timeline
+  }
   
   timeline    
     .from(projectNextLink.value, {
       autoAlpha: 0,
     })
-    .from(context.selector?.('.project-next-link__label__line'), {
-      y: '100%',
-      duration: .4,
-      ease: "power3.out",
-      stagger: .1
-    })
+    .add(splittedOvertitleTimeline)
+    .add(splittedTitleTimeline, '<+=.1')
 }, projectNextLink)
 </script>
 
 <template>
-<div ref="projectNextLink" class="autoalpha">
+<div ref="projectNextLink" class="project-next-link-wrapper autoalpha">
   <NuxtLink
+    @mouseenter="handleHoverFocus"
+    @focus="handleHoverFocus"
     :to="`/projets/${slug}`"
     class="project-next-link">
     <CustomPicture 
@@ -48,14 +98,11 @@ useGSAP((isReducedMotion, context) => {
       :pictureDataDefault="coverImg"
       :loading="loadAssets" />
 
-    <div class="project-next-link__label">
-      <div class="project-next-link__label__line-wrapper">
-        <span class="project-next-link__label__line font-mono--base">Projet suivant</span>
-      </div>
-
-      <div class="project-next-link__label__line-wrapper">
-        <span class="project-next-link__label__line font-sans--md-capitalized">{{ title }}</span>
-      </div>
+    <div class="project-next-link__label">    
+      <span class="sr-only">Projet suivant {{ title }}</span>
+      
+      <div class="project-next-link__label__overtitle font-mono--base">Projet suivant</div>
+      <div class="project-next-link__label__title font-sans--md-capitalized">{{ title }}</div>
     </div>
   </NuxtLink>
 </div>
